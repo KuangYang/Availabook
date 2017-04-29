5b3c7bf46385e3e5887beb9b014d9a0656dc7b40
Kuang Yang <ky2342@columbia.edu>
2017-04-29 19:14:17 -0400

    Signin and Signout Popup

---
+++ Asite/availabook/templates/js/availabook.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    console.log("ready!");
+
+    // Get the modal and when the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        var modals = document.getElementsByClassName('modal');
+        for (var i = 0; i <  modals.length; i++) {
+            if (event.target == modals[i]) {
+            modals[i].style.display = "none";
+            }
+        }
+    };
+});
+
+
+
