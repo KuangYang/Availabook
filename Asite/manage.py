e11062a508a30fb0f80687c889797e89560c0fab
Kaili Chen <kc3031@columbia.edu>
2017-05-01 14:17:52 -0400

    import credential

--- Asite/manage.py
+++ Asite/manage.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python
 import os
 import sys
+import json
+
+"""import credentials from root/AppCreds"""
+with open(os.path.dirname(sys.path[0])+'/AppCreds/AWSAcct.json','r') as AWSAcct:
+    awsconf = json.loads(AWSAcct.read())
+aws_access_key_id=awsconf["aws_access_key_id"],
+aws_secret_access_key=awsconf["aws_secret_access_key"]
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Asite.settings")
