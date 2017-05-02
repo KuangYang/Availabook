d62ba97b96e53fbe861149ccbe5a031e17a7752a
jiamin huang <jh3768@columbia.edu>
2017-05-01 20:51:16 -0400

    redirect

--- Asite/availabook/static/js/availabook.js
+++ Asite/availabook/static/js/availabook.js
@@ -13,4 +13,38 @@ $(document).ready(function() {
 });
 
 
-
+function login(formData) {
+    id = formData['id']
+    psw = formData['psw']
+    cleanData = {};
+    cleanData['info'] = {
+        'id':       id,
+        'password': psw]
+    }
+    console.log(id);
+    
+    $.ajax({
+        type: "POST",
+        url: 'https://127.0.0.1/availabook/login',
+        crossDomain: true,
+        contentType: 'application/json',
+        data: JSON.stringify(cleanData),
+        dataType: 'json',
+        success: function(service_data){
+           if (service_data['status']=='success'){
+               accountDisplayHandler.logIn(formData.email);
+               $('#loginModal').modal('hide')
+               //set jwt_token
+               setCookie("jwt_token", service_data['jwt']);
+           }
+           else{
+               alert("Invalid email or password.");
+               accountDisplayHandler.logOut();
+           }
+        },
+        error: function (e) {
+           alert("error");
+           accountDisplayHandler.logOut();
+        }
+    });
+}
--- Asite/availabook/views.py
+++ Asite/availabook/views.py
@@ -1,6 +1,8 @@
 #from django.shortcuts import render
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib import messages
+from django.core import serializers
 
 from availabook.models import User
 
@@ -8,17 +10,20 @@ from availabook.models import User
 def index(request):
     ''' render homepage'''
     print "reder index"
+    print "username " + request.user.username
     return render(request, 'index.html')
 
-def login(request):
+def login(request, onsuccss = '/availabook/', onfail = '/availabook/'):
  	user_id = request.POST.get("id")
  	pwd = request.POST.get("psw")
 
  	user = User(user_id, pwd)
  	if user.authen_user():
  		user.authorize()
- 		return render(request, 'index.html')
+ 		return redirect(onsucess)
  	else:
  		#alert("User Information Not exists")
- 		return render(request, 'index.html')
+ 		messages.add_message(request, messages.ERROR, 'Login Failed. Try again.', 'login', True)
+ 		print messages
+ 		return redirect(onfail)
 
