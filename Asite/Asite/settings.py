47b5fc0d84c3f5a1e7a6e417a53651bdd86023a6
jiamin huang <jh3768@columbia.edu>
2017-05-01 22:05:14 -0400

    login

--- Asite/Asite/settings.py
+++ Asite/Asite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
 )
 AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
